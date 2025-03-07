b18ce302ba2e1aa417e930d332b075b42bd0f863
MelonTe <minatolymik@gmail.com>
2025-03-07 17:50:06 +0800

    后端加入统一响应格式、设置错误码

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,16 +2,27 @@ package router
 
 //全局路由注册
 import (
+	"shg/router/v1" // 导入 v1 路由
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
-	"shg/router/v1" // 导入 v1 路由
 )
 
 // SetupRoutes 全局路由设置
 func SetupRoutes(r *gin.Engine) {
 	//注册swagger
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	//启用 CORS 中间件，允许跨域资源共享
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},                                                 // 允许的来源（前端地址）
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},           // 允许的 HTTP 方法
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"}, // 允许的请求头
+		ExposeHeaders:    []string{"Content-Length", "Authorization"},                   // 允许暴露的响应头
+		AllowCredentials: false,                                                         // 是否允许携带凭证（如 Cookies）
+		AllowWildcard:    true,                                                          // 是否允许任何来源
+	}))
 	// 注册 v1 路由
 	v1.RegisterV1Routes(r)
 }
--- backend/router/v1/router.go
+++ backend/router/v1/router.go
@@ -1,9 +1,12 @@
 package v1
 
 import (
+	"shg/internal/controller"
+
 	"github.com/gin-gonic/gin"
 )
 
 // RegisterV1Routes 注册 v1 版本的路由
 func RegisterV1Routes(r *gin.Engine) {
+	r.GET("/v1/hello", controller.HelloWorld)
 }
