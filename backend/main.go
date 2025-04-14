dd1f4a21ed2e37e13bb36b283e6193e79602bb0e
MelonTe <minatolymik@gmail.com>
2025-04-15 01:15:57 +0800

    完成项目整体开发

--- backend/main.go
+++ backend/main.go
@@ -27,27 +27,3 @@ import (
 func main() {
 	cmd.Main()
 }
-
-func countGoodNumbers(n int64) int {
-	ans := int64(1)
-	const mod = 1e9 + 7
-	once := int64(20) //每两个数的组合
-	times := n / 2    //once ^ times
-	//快速幂运算
-	pow := func(x, y int64) int64 {
-		res := int64(1)
-		for y != 0 {
-			if y&1 == 1 {
-				res = res * x % mod
-			}
-			x = x * x % mod
-			y >>= 1
-		}
-		return res
-	}
-	ans = pow(once, times)
-	if n%2 == 1 {
-		ans = ans * 5 % mod
-	}
-	return int(ans)
-}
